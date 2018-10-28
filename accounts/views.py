fc6a0714020e1eeb1f005267765e7c44fca88b46
gelos12 <gelos2012@gmail.com>
2018-10-28 23:17:37 +0900

    유저 검색 필터링 버그 수정

--- accounts/views.py
+++ accounts/views.py
@@ -6,14 +6,15 @@ from . import serializers
 #유저 검색 필터링
 class UserListFilterBackend(filters.BaseFilterBackend):
     def filter_queryset(self, request, queryset, view):
-        flt = {}
+        flt = {}        
         for param in request.query_params:
-            for fld in view.filter_fields:
-                if param == 'email':
-                    user = models.User.objects.filter(email=request.query_params[param]).first()
-                    flt[param] = user
-                elif param.startswith(fld):
-                    flt[param] = request.query_params[param]
+            if param == 'email':
+                user = models.User.objects.filter(email=request.query_params[param]).first()
+                flt['email'] = user.email
+            else:
+                for fld in view.filter_fields:
+                    if param.startswith(fld):
+                        flt[param] = request.query_params[param]
         return queryset.filter(**flt)
 
 
